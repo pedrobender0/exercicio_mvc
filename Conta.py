a5178e68676e197d6e552c4f0d7df3fa3aaba577
pedrobender0 <pedrobenderprado@gmail.com>
2026-03-20 11:08:52 -0300

    clonagem repositorio

---
+++ Conta.py
@@ -0,0 +1,26 @@
+class Transacao:
+    def registrar():
+
+class Historico:
+    def __init__(self, transacao):
+        self.transacao = transacao
+
+    def adicionar_transacao():
+
+
+class Cliente:
+    def __init__(self, endereco, contas):
+        self.endereco = endereco
+        self.contas = contas
+
+class Conta:
+    def __init__(self, saldo, numero,agencia):
+        self.saldo = saldo
+        self.numero = numero
+        self.agencia = agencia
+        self.cliente = Cliente
+    def versaldo(self, saldo):
+        print(self.saldo)
+    def
+
+
